af2388cf101d356a4ecfad281b7ba054e8d19a13
Guille Mesyngier <gmesyngier@hotmail.com>
2015-07-02 04:05:54 -0300

    scrollpage fix

--- components/tenaris/sections.js
+++ components/tenaris/sections.js
@@ -1,6 +1,6 @@
 $(function(){
   $(document).on ('click','.goto',function() {
-    var id = $(this).attr('id');
+    var id = $(this).attr('data-name');
     $('.page').animate({
       scrollTop: $("#"+id).offset().top
     }, 1000);
