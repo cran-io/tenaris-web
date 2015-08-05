b3d14f4f16be7b9eaf02631df889b5b7efad752d
Guille Mesyngier <gmesyngier@hotmail.com>
2015-08-05 19:57:24 -0300

    add animation color and logo use

--- script/controllers/logoUseController.js
+++ script/controllers/logoUseController.js
@@ -4,20 +4,20 @@ app.controller('logoUseController', function(menuFactory, $scope){
 	menuFactory.makeIt();
 
 	$(".slider-control").click(function(e) {
-        e.preventDefault();
+                e.preventDefault();
 
-        var slideId = $(this).attr('href');
-        var currentSwitch = $(this);
-        var switchs = $("a.slider-control");
-        var	pages = $('.slider-wrapper .slide');
-        var currentPage = $('.slide'+slideId);
-        
-        if (!currentPage.hasClass('active')){
-        	pages.removeClass('active');
-        	currentPage.addClass('active');
-		    switchs.removeClass('active');
-		   	currentSwitch.addClass('active');	
-        }
+                var slideId = $(this).attr('href');
+                var currentSwitch = $(this);
+                var switchs = $("a.slider-control");
+                var pages = $('.slider-wrapper .slide');
+                var currentPage = $('.slide'+slideId);
+                
+                if (!currentPage.hasClass('active')){
+                	pages.removeClass('active');
+                	currentPage.addClass('active');
+        		    switchs.removeClass('active');
+        		   	currentSwitch.addClass('active');	
+                }
 
 	});
 });
